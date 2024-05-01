8aaa8968cd9bee8170adc63eed820c2848c22cc9
Anthony Grasso <UNDEFINED>
2024-05-01 10:05:13 +1000

    Added ability to specify or ignore keyspaces
    * Added argument to schema argument parser to allow the parsing of dse, OpsCenter, solr, and system* keyspaces
    * Modified the process_schema method to allow the reserved keyspace parsing flag to be passed via a keywoard argument
    * Modifed the ignore keyspace argument in argument parser to be an empty list
    * Modifications to SchemaParser class
    * Modified the parse_schema method to allow reserved keyspace parsing flag to be passed via a keywoard argument
    * Added full list of the reserved keyspaces in DSE and Cassandra
    * Modified parse_schema method logic to populate the ignore or select keyspace sets with the reserved keyspaces depending on the state of parser reserved keyspaces flag
    * Added KS_OK, KS_UNSELECTED, and KS_IGNORED keyspace states
    * Added __do_we_process_keyspace method to check if the keyspace should be parsed based on the ignore and select keyspaces sets and return a keyspace state
    * Renamed the keyspaces attribute to parsed_keyspaces and changed its type a dictionary to keep track of keyspaces processed and their state
    * Modified _parse_use_statement method logic to just set current keyspace attribute, and removed the call to the _set_keyspace method
    * Modified _parse_create_keyspace_statement method logic to set current keyspace attribute, call the _do_we_process_keyspace and store the result in the parsed_keyspaces attribute, and removed the call to the __set_keyspace method
    * Removed the unused __set_keyspace method
    * Modified __set_table method logic to use the parsed_keyspaces attribute to determine how to process the keyspace, and set the keyspace and table name attributes accordingly
    * Modified the __parse_create_table_statement method to check whether the keyspace and table attributes are set before parsing rather than checking the keyspace itself
    * Added __parser_lwt_statement method to parse keyspace and table conditional creation
    * Renamed set_table method toresolve_table_name to better reflect what its logic does
    * Modified resolve_table_name method to return the table name parts or None, rather than set them in the current_table_parts attributed
    * Modified parse_create_table_statement method to check the returned table name parts before processing the table
    * Removed current_table_parts attribute from class as it was no longer used
    * Added keyspace_status_messages dictionary to class to map keyspace status values to a message that can be printed in a log
    * Added logging module to schema_parser
    * Added logger creation and configuration to init method in class
    * Modified all print messages to use logger attribute in class
    * Removed ERROR prefix from exception messages
    * Renamed do_we_process_keyspace method to get_keyspace_status to better reflect what its logic does

--- cql_schema_analyser/schema_parser.py
+++ cql_schema_analyser/schema_parser.py
@@ -1,8 +1,37 @@
 import copy
+import logging
 import re
 
 
 class SchemaParser:
+    KS_OK = 0
+    KS_UNSELECTED = 1
+    KS_IGNORED = 2
+
+    keyspace_status_messages = {
+        KS_OK: 'Tables without a keyspace defined will assumed to be in this keyspace when processed.',
+        KS_UNSELECTED: 'Keyspace is unselected. Tables without a keyspace defined will assume to be in this '
+                       'keyspace and ignored.',
+        KS_IGNORED: 'Keyspace is being ignored. Tables without a keyspace defined will assume to be in this '
+                    'keyspace and ignored.'
+    }
+
+    reserved_keyspaces = {
+        'dse_insights',
+        'dse_insights_local',
+        'dse_leases',
+        'dse_security',
+        'dse_system',
+        'dse_system_local',
+        'OpsCenter',
+        'solr',
+        'system',
+        'system_auth',
+        'system_distributed',
+        'system_schema',
+        'system_traces'
+    }
+
     column_grammar_to_state_mapping = {
         'ascii': 'COLUMN_TYPE_DEF',
         'bigint': 'COLUMN_TYPE_DEF',
@@ -42,11 +71,20 @@ class SchemaParser:
 
     def __init__(self):
         self.current_keyspace = ''
-        self.current_table_parts = ()
-        self.keyspaces = set()
+        self.parsed_keyspaces = {}
+        self.ignore_keyspace = None
+        self.select_keyspace = None
+        self.parse_reserved_keyspaces = False
         self.statement_raw = ''
         self.statement_terms = []
         self.dom = []
+
+        self.scope = ''
+        self.scope_stack = 0
+        self.column_def_stack = 0
+        self.column_name = ''
+        self.column_type = ''
+        self.column_type_lookup = {}
 
         self.parse_statement_callback = {
             'create': self.__parse_create_statement,
@@ -58,6 +96,18 @@ class SchemaParser:
             'table': self.__parse_create_table_statement,
         }
 
+        self.logger = logging.getLogger(__name__)
+        logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
+
+    def __reset_parser_state(self):
+        self.current_keyspace = ''
+        self.parsed_keyspaces = set()
+        self.ignore_keyspace = None
+        self.select_keyspace = None
+        self.statement_raw = ''
+        self.statement_terms = []
+        self.dom = []
+
         self.scope = ''
         self.scope_stack = 0
         self.column_def_stack = 0
@@ -65,48 +115,68 @@ class SchemaParser:
         self.column_type = ''
         self.column_type_lookup = {}
 
-    def __reset_parser_state(self):
-        self.current_keyspace = ''
-        self.keyspaces = set()
-        self.table = ''
-        self.statement_raw = ''
-        self.statement_terms = []
-        self.dom = []
+    def __get_keyspace_status(self, keyspace):
+        if self.select_keyspace:
+            if keyspace not in self.select_keyspace:
+                return SchemaParser.KS_UNSELECTED
+        elif self.ignore_keyspace:
+            if keyspace in self.ignore_keyspace:
+                return SchemaParser.KS_IGNORED
 
-    def __set_keyspace(self):
+        return SchemaParser.KS_OK
+
+    def __resolve_table_name(self):
         term_item = self.statement_terms.pop()
-        if term_item[0].isalnum() and term_item.lower() != 'with':
-            print('Setting keyspace to "{}".'.format(term_item))
-            self.keyspaces.add(term_item)
-            self.current_keyspace = term_item
+
+        if self.statement_terms[-1] == '.':
+            keyspace_name = term_item
+            self.statement_terms.pop()
+            table_name = self.statement_terms.pop()
+        elif self.statement_terms[-1] == '(':
+            keyspace_name = self.current_keyspace
+            table_name = term_item
         else:
-            raise ValueError('ERROR: Malformed keyspace definition. Skipping.')
+            raise ValueError(
+                'Malformed "CREATE TABLE" statement found. Expecting keyspace table dot separator when statement '
+                'format is CREATE TABLE [IF NOT EXISTS] <keyspace>.<table> ( ... ), or  column definition opening '
+                'bracket when statement format is CREATE TABLE [IF NOT EXISTS] <table> ( ... ). Found "{}" instead. '
+                'Ignoring.'.format(self.statement_terms[-1]))
 
-    def __set_table(self):
-        term_item = self.statement_terms.pop()
+        if not table_name[0].isalnum():
+            raise ValueError('Malformed "CREATE TABLE" statement found. Expecting table name to begin with an'
+                             'alphanumeric character. Found table name "{}". Skipping.'.format(table_name))
 
-        # Check if the table name is in the format <keyspace>.<table> and <keyspace> matches the current keyspace
-        if term_item in self.keyspaces:
-            separator = self.statement_terms.pop()
-            if separator == '.':
-                self.current_table_parts = (term_item, self.statement_terms.pop())
+        if keyspace_name in self.parsed_keyspaces:
+            keyspace_status = self.parsed_keyspaces[keyspace_name]
+            if keyspace_status == SchemaParser.KS_OK:
+                return keyspace_name, table_name
             else:
-                raise ValueError('ERROR: Malformed "CREATE TABLE" statement found. Expecting table name to be in '
-                      'format <keyspace>.<table>. Dot separator is missing. Skipping.')
-        # Assume we just have a table name in the current keyspace
+                if keyspace_status == SchemaParser.KS_IGNORED:
+                    self.logger.info('Table "{}.{}" defined in CREATE statement. Table is in an ignored keyspace '
+                                     'and will be ignored.'.format(keyspace_name, table_name))
+                elif keyspace_status == SchemaParser.KS_UNSELECTED:
+                    self.logger.info('Table "{}.{}" defined in CREATE statement. Table is in an unselected keyspace'
+                                     'and will be ignored.'.format(keyspace_name, table_name))
         else:
-            if self.statement_terms[-1] == 'START_SCOPE_TABLE_DEF':
-                self.current_table_parts = (self.current_keyspace, term_item)
-            elif self.statement_terms[-1] == '.':
-                self.statement_terms.pop()
-                raise ValueError('ERROR: Unexpected keyspace "{}" found for table "{}". Skipping'.format(
-                    term_item,
-                    self.statement_terms.pop())
+            self.logger.warn('Table "{}.{}" is in an undefined keyspace. Ignoring CREATE statement.'.format(
+                keyspace_name,
+                table_name
+            ))
+
+        return None
+
+    def __parser_lwt_statement(self):
+        lwt_clauses = ['IF', 'NOT', 'EXISTS']
+        for idx, val in enumerate(lwt_clauses):
+            if self.statement_terms.pop() != val:
+                raise ValueError(
+                    'Malformed "CREATE KEYSPACE" statement found. '
+                    'Expecting "{}" after "{}" Skipping.'.format(val, lwt_clauses[idx - 1])
                 )
 
     def __parse_create_statement(self):
         if len(self.statement_terms) < 2:
-            print('ERROR: Incomplete "CREATE" statement found. Skipping.')
+            self.logger.error('Incomplete "CREATE" statement found. Ignoring.')
             return
 
         create_statement = self.statement_terms.pop().lower()
@@ -114,34 +184,53 @@ class SchemaParser:
         try:
             self.parse_create_statement_callback[create_statement]()
         except KeyError:
-            print('ERROR: Unsupported CREATE statement "{}" found. Skipping.'.format(create_statement))
+            self.logger.error('Unsupported CREATE statement "{}" found. Ignoring.'.format(create_statement))
 
     def __parse_use_statement(self):
-        if len(self.statement_terms) < 2:
-            print('ERROR: Incomplete "USE" statement found. Skipping.')
-            return
-
-        try:
-            self.__set_keyspace()
-        except ValueError as e:
-            print(e)
+        term_item = self.statement_terms.pop()
+        if term_item[0].isalnum():
+            if term_item in self.parsed_keyspaces:
+                self.current_keyspace = term_item
+                self.logger.info('Keyspace "{}" selected in USE statement. {}'.format(
+                    term_item,
+                    self.keyspace_status_messages[self.parsed_keyspaces[term_item]]
+                ))
+            else:
+                self.logger.warning('Keyspace "{}" is undefined. Ignoring USE statement.'.format(term_item))
+        else:
+            raise ValueError('Malformed USE KEYSPACE statement. Expecting keyspace name; found "{}".'.format(term_item))
 
     def __parse_create_keyspace_statement(self):
-        if len(self.statement_terms) < 3:
-            print('ERROR: Incomplete "CREATE KEYSPACE" statement found. Skipping.')
-            return
+        if self.statement_terms[-1] == 'IF':
+            try:
+                self.__parser_lwt_statement()
+            except ValueError as e:
+                self.logger.error(e)
+                return
 
-        try:
-            self.__set_keyspace()
-        except ValueError as e:
-            print(e)
+        term_item = self.statement_terms.pop()
+        if term_item[0].isalnum() and term_item.lower() != 'with':
+            if term_item in self.parsed_keyspaces:
+                raise ValueError('Keyspace "{}" already defined. Skipping.'.format(term_item))
+
+            process_rtn_val = self.__get_keyspace_status(term_item)
+            self.parsed_keyspaces[term_item] = process_rtn_val
+            self.current_keyspace = term_item
+            self.logger.info('Keyspace "{}" defined in CREATE statement. {}'.format(
+                term_item,
+                self.keyspace_status_messages[process_rtn_val]
+            ))
+        else:
+            raise ValueError('Malformed CREATE KEYSPACE statement. Expecting keyspace name; found "{}".'.format(
+                term_item
+            ))
 
     def __parse_create_table_statement_column_def(self, current_state, previous_state, term_item, dom_object):
         if current_state == 'START_SCOPE_TABLE_DEF':
             self.scope_stack += 1
             if previous_state == 'KEY_CLAUSE':
                 if len(dom_object['attributes']['key']['partition']):
-                    raise ValueError('ERROR: Multiple primary keys found. Skipping.')
+                    raise ValueError('Multiple primary keys found. Skipping.')
                 else:
                     self.scope = 'PARTITION_KEY_DEF'
         elif current_state == 'COLUMN_NAME_DEF':
@@ -165,21 +254,20 @@ class SchemaParser:
                 dom_object['attributes']['columns'].append(self.column_type)
                 if previous_state == 'KEY_CLAUSE':
                     if len(dom_object['attributes']['key']['partition']):
-                        raise ValueError('ERROR: Multiple primary keys found. Skipping.')
+                        raise ValueError('Multiple primary keys found. Skipping.')
                     else:
                         dom_object['attributes']['key']['partition'].append(self.column_type)
         elif current_state == 'PRIMARY_CLAUSE':
             pass
         elif current_state == 'KEY_CLAUSE':
             if previous_state != 'PRIMARY_CLAUSE':
-                raise ValueError('ERROR: Unexpected "KEY" clause found. Skipping.')
+                raise ValueError('Unexpected "KEY" clause found. Skipping.')
         elif current_state == 'END_SCOPE_TABLE_DEF':
             self.scope_stack -= 1
             dom_object['attributes']['columns'].append(self.column_type)
             if self.scope_stack == 0:
                 if not len(dom_object['attributes']['key']['partition']):
-                    raise ValueError('ERROR: No primary key found. Skipping.')
-
+                    raise ValueError('No primary key found. Skipping.')
 
     def __parse_create_table_statement_partition_def(self, current_state, previous_state, term_item, dom_object):
         if current_state == 'COLUMN_NAME_DEF':
@@ -187,7 +275,7 @@ class SchemaParser:
         elif current_state == 'START_SCOPE_TABLE_DEF':
             self.scope_stack += 1
             if self.scope_stack > 3:
-                raise ValueError('ERROR: Unexpected "(" found. Skipping.')
+                raise ValueError('Unexpected "(" found. Skipping.')
         elif current_state == 'TERM_SEPARATOR':
             dom_object['attributes']['key']['partition'].append(self.column_type_lookup[self.column_name])
             if self.scope_stack == 2:
@@ -212,7 +300,7 @@ class SchemaParser:
                 dom_object['attributes']['key']['clustering'].append(self.column_type_lookup[self.column_name])
                 self.scope = 'TABLE_COLUMN_DEF'
             else:
-                raise ValueError('ERROR: Unexpected ")" found. Skipping.')
+                raise ValueError('Unexpected ")" found. Skipping.')
 
     def __parse_create_table_statement_columns(self, dom_object):
         term_item = self.statement_terms.pop()
@@ -222,7 +310,7 @@ class SchemaParser:
             self.scope = 'TABLE_COLUMN_DEF'
             self.scope_stack = 1
         else:
-            print('ERROR: Malformed "CREATE TABLE" statement found. Expecting table definition to be in the format '
+            print('Malformed "CREATE TABLE" statement found. Expecting table definition to be in the format '
                   '("<field_name> <field_type>, ..."). Skipping.')
             return
 
@@ -244,22 +332,24 @@ class SchemaParser:
             previous_state = current_state
 
     def __parse_create_table_statement(self):
-        # Resolve the table name first before we check if we should skip this table
+        if self.statement_terms[-1] == 'IF':
+            try:
+                self.__parser_lwt_statement()
+            except ValueError as e:
+                self.logger.error(e)
+                return
+
         try:
-            self.__set_table()
+            table_name_parts = self.__resolve_table_name()
         except ValueError as e:
-            print(e)
+            self.logger.error(e)
             return
 
-        if self.current_table_parts[0].split('_')[0].lower() in ['dse', 'opscenter', 'solr', 'system']:
-            print('Table "{}" belongs to keyspace "{}". Skipping.'.format(
-                self.current_table_parts[1],
-                self.current_table_parts[0])
-            )
+        if not table_name_parts:
             return
 
         dom_object = {
-            'name': '{}.{}'.format(self.current_table_parts[0], self.current_table_parts[1]),
+            'name': '{}.{}'.format(table_name_parts[0], table_name_parts[1]),
             'attributes': {
                 'columns': [],
                 'key': {
@@ -274,18 +364,29 @@ class SchemaParser:
         try:
             self.__parse_create_table_statement_columns(dom_object)
         except ValueError as e:
-            print(e)
+            self.logger.error(e)
             return
 
         term_item = self.statement_terms.pop()
 
-        if term_item in ['WITH', ';']:
+        if term_item.lower() in ['with', ';']:
             self.dom.append(dom_object)
         else:
-            print('ERROR: Malformed "CREATE TABLE" statement found. Expecting table properties to be in the format '
-                  'CREATE TABLE ... (...) WITH (<property>, ...); or CREATE TABLE ... (...);. Skipping.')
+            self.logger.error('Malformed "CREATE TABLE" statement found. Expecting table properties to be in the '
+                              'format CREATE TABLE ... (...) WITH (<property>, ...); or CREATE TABLE ... (...);. '
+                              'Ignoring.')
 
-    def parse_schema(self, cql_file_path):
+    def parse_schema(self, cql_file_path, ignore_keyspace=None, select_keyspace=None, parse_reserved_keyspaces=False):
+        if ignore_keyspace:
+            self.ignore_keyspace = set(ignore_keyspace)
+            if not parse_reserved_keyspaces:
+                self.ignore_keyspace = self.ignore_keyspace.union(SchemaParser.reserved_keyspaces)
+        if select_keyspace:
+            self.select_keyspace = set(select_keyspace)
+            if parse_reserved_keyspaces:
+                self.select_keyspace = self.select_keyspace.union(SchemaParser.reserved_keyspaces)
+
+        self.logger.info('Parsing CQL schema file "{}"'.format(cql_file_path))
         with open(cql_file_path, 'r') as in_cql:
             for raw_line in in_cql:
                 statement_part = raw_line.strip()
@@ -307,7 +408,7 @@ class SchemaParser:
                     try:
                         self.parse_statement_callback[cql_operation]()
                     except KeyError:
-                        print('ERROR: Unsupported CQL operation "{}" found. Skipping.'.format(cql_operation))
+                        self.logger.error('Unsupported CQL operation "{}" found. Skipping.'.format(cql_operation))
 
                     self.statement_raw = ''
                     self.statement_terms = []
--- cql_schema_analyser/schema_processor.py
+++ cql_schema_analyser/schema_processor.py
@@ -20,9 +20,13 @@ class SchemaProcessor:
         self.cql_schema_paser = schema_parser.SchemaParser()
         self.cql_table_template_analyser = table_template_analyser.TableTemplateAnalyser()
 
-    def process_schema(self, schema_file):
-        print('Parsing schema file: {}'.format(schema_file))
-        cql_schema_dom = self.cql_schema_paser.parse_schema(schema_file)
+    def process_schema(self, schema_file, ignore_keyspace=None, select_keyspace=None, parse_reserved_keyspaces=False):
+        cql_schema_dom = self.cql_schema_paser.parse_schema(
+            schema_file,
+            ignore_keyspace=ignore_keyspace,
+            select_keyspace=select_keyspace,
+            parse_reserved_keyspaces=parse_reserved_keyspaces
+        )
 
         for dom_obj in cql_schema_dom:
             self.cql_table_template_analyser.catalog_table_definition(dom_obj)
@@ -33,10 +37,45 @@ class SchemaProcessor:
     def main_cli():
         arg_parser = argparse.ArgumentParser(description='Process a CQL schema file and output the table definitions.')
         arg_parser.add_argument('schema_file', help='The CQL schema file to process.')
+        arg_parser.add_argument(
+            '-i',
+            '--ignore-keyspace',
+            dest='ignore_keyspace',
+            nargs='+',
+            default=[],
+            help='One or more keyspaces to ignore when processing the schema. All tables in that keyspace will be '
+                 'ignored. The --select-keyspace option takes precedence over this option. That is, if a keyspace is '
+                 'specified in both this and the --select-keyspace option, it will be processed rather than ignored. '
+        )
+        arg_parser.add_argument(
+            '-s',
+            '--select-keyspace',
+            dest='select_keyspace',
+            nargs='+',
+            default=[],
+            help='One or more keyspaces to exclusively select when processing the schema. All other keyspaces and '
+                 'their associated tables will be ignored. This option takes precedence over the --ignore-keyspace '
+                 'option. That is, if a keyspace is specified in both this and the --ignore-keyspace option, it will '
+                 'be processed rather than ignored. Defaults to all keyspaces being processed except for those listed '
+                 'in the --ignore_keyspace option.'
+        )
+        arg_parser.add_argument(
+            '-r',
+            '--parse-reserved-keyspaces',
+            dest='parse_reserved_keyspaces',
+            action='store_true',
+            help='Parse the reserved dse*, OpsCenter, solr, and system* keyspaces. '
+                 'By default, these keyspaces are ignored.'
+        )
         schema_proc_args = arg_parser.parse_args()
 
         schema_processor = SchemaProcessor()
-        schema_processor.process_schema(schema_proc_args.schema_file)
+        schema_processor.process_schema(
+            schema_proc_args.schema_file,
+            ignore_keyspace=schema_proc_args.ignore_keyspace,
+            select_keyspace=schema_proc_args.select_keyspace,
+            parse_reserved_keyspaces=schema_proc_args.parse_reserved_keyspaces
+        )
 
 
 if __name__ == '__main__':
